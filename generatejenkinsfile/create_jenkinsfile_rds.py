8999d8e8fa79ab9135d66e37083de6f52fb1e19a
Andrew Soltes <soltesandrew@gmail.com>
2023-04-04 18:12:27 +0800

    working jinja2 template

--- generatejenkinsfile/create_jenkinsfile_rds.py
+++ generatejenkinsfile/create_jenkinsfile_rds.py
@@ -19,18 +19,17 @@ def get_rds_instance_names():
     rds_client = boto3.client('rds', region_name=args.region)
     response = rds_client.describe_db_instances()
     instance_names = [instance['DBInstanceIdentifier'] for instance in response['DBInstances']]
-    print (instance_names)
     return instance_names
 
 
-def render_groovy_template(instance_names):
+def render_groovy_template(instance_names, aws_account_number):
     env = Environment(loader=FileSystemLoader(os.path.dirname(__file__)))
     template = env.get_template('groovy_template.j2')
-    output = template.render(instance_names=instance_names)
+    output = template.render(instance_names=instance_names, aws_account_number=aws_account_number)
     return output
 
 def save_groovy_script(output):
-    with open(f'{directory_name}/Jenkinsfile', 'w') as file:
+    with open(f'{aws_account_id}/Jenkinsfile', 'w') as file:
         file.write(output)
 
 
@@ -39,9 +38,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate RDS snapshot Jenkinsfile')
     parser.add_argument('-r', '--region', metavar='region', type=str, help='specify the rds region')
     args = parser.parse_args()
-    directory_name = get_aws_account_number()
-    create_directory_if_not_exists(directory_name)
+    aws_account_id = get_aws_account_number()
+    create_directory_if_not_exists(aws_account_id)
     instance_names = get_rds_instance_names()
-    groovy_script = render_groovy_template(instance_names)
+    groovy_script = render_groovy_template(instance_names, aws_account_id)
     save_groovy_script(groovy_script)
-    print(f"Generated Jenkinsfile is now on {directory_name}")
+    print(f"Generated Jenkinsfile is now on {aws_account_id}")
